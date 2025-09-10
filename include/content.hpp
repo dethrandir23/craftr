4ed311f42ccfad0c4be165b8de55ed3a7ed95218
Bekir Efe Öztürk <efeoztrk23@proton.me>
2025-09-10 22:03:52 +0300

    Add content.hpp to project

---
+++ include/content.hpp
@@ -0,0 +1,15 @@
+//content.hpp
+#pragma once
+
+#include <string>
+
+class Content
+{
+private:
+    std::string text;
+public:
+    Content() = default;
+    Content(const std::string& text) { this->text = text; }
+    const std::string get_text() const { return text; }
+    void set_text(std::string new_text) { this->text = new_text; }
+};
