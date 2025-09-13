ad5ef72ac7ae649da564aed3a21e0e225831d5da
Bekir Efe Öztürk <efeoztrk23@proton.me>
2025-09-13 21:04:34 +0300

    feat: add LicenseUtils module with initial implementation

---
+++ include/license_utils.hpp
@@ -0,0 +1,11 @@
+//license_utils.hpp
+#pragma once
+#include "licensetype.hpp"
+#include <string>
+
+namespace LicenseUtils
+{
+    LicenseType GetLicenseType(const std::string& name);
+
+    std::string GetLicenseText(const LicenseType& licenseType);
+}
---
+++ src/license_utils.cpp
@@ -0,0 +1,59 @@
+//license_utils.cpp
+#include "../include/file_utils.hpp"
+#include "../include/licensetype.hpp"
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+namespace LicenseUtils
+{
+    LicenseType GetLicenseType(const std::string& name)
+    {
+        std::string licenseType = name;
+        std::transform(licenseType.begin(), licenseType.end(), licenseType.begin(), ::tolower);
+
+        if (licenseType == "mit") {
+            return LicenseType::MIT;
+        } else if (licenseType == "bsd") {
+            return LicenseType::BSD;
+        } else if (licenseType == "gpl") {
+            return LicenseType::GPL;
+        } else if (licenseType == "apache") {
+            return LicenseType::Apache;
+        } else {
+            return LicenseType::Custom;
+        }
+
+    }
+
+    std::string GetLicenseText(const LicenseType& licenseType)
+    {
+        std::string licensePath;
+
+        switch (licenseType) {
+            case LicenseType::MIT:
+                licensePath = "licenses/MIT.txt";
+                break;
+            case LicenseType::BSD:
+                licensePath = "licenses/BSD.txt";
+                break;
+            case LicenseType::GPL:
+                licensePath = "licenses/GPL.txt";
+                break;
+            case LicenseType::Apache:
+                licensePath = "licenses/APACHE.txt";
+                break;
+            case LicenseType::Custom:
+                return "";
+            default:
+                return "";
+        }
+
+        std::filesystem::path fullPath = FileUtils::get_templates_folder() / licensePath;
+
+        std::cout << "Loading license from: " << fullPath << std::endl;
+        std::cout << "File exists: " << std::filesystem::exists(fullPath) << std::endl;
+
+        return FileUtils::LoadFileToString(fullPath.string());
+    }
+}
