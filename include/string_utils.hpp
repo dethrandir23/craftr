1da262fcb39a28c824f63f69407f287f535513fc
Bekir Efe Öztürk <efeoztrk23@proton.me>
2025-09-16 13:59:19 +0300

    refactor: remove all unused old functions and enums from string_utils module.

--- include/string_utils.hpp
+++ include/string_utils.hpp
@@ -1,50 +1,8 @@
 // string_utils.hpp
 #pragma once
 
-#include "licensetype.hpp"
 #include <string>
-#include <unordered_map>
-
 namespace StringUtils {
-
-enum class Placeholder { ProjectName, AuthorName, Date };
-
-inline std::string ToString(Placeholder ph) {
-  switch (ph) {
-  case Placeholder::ProjectName:
-    return "{{PROJECT_NAME}}";
-  case Placeholder::AuthorName:
-    return "{{AUTHOR_NAME}}";
-  case Placeholder::Date:
-    return "{{DATE}}";
-  }
-  return "";
-}
-
-enum class ContentType { CMake, Readme, Metadata, Main, License };
-
-inline std::string ToString(ContentType ct) {
-  switch (ct) {
-  case ContentType::CMake:
-    return "cmake";
-  case ContentType::Readme:
-    return "readme";
-  case ContentType::Metadata:
-    return "metadata";
-  case ContentType::Main:
-    return "main";
-  case ContentType::License:
-    return "license";
-  }
-  return "";
-}
-
-using Contents = std::unordered_map<ContentType, std::string>;
-using ReplacePairs = std::unordered_map<Placeholder, std::string>;
-
-ReplacePairs GetReplacePairs(const std::string &project_name,
-                             const std::string &author_name);
-Contents GetContents(const LicenseType &license);
 
 std::string toLower(std::string s);
 } // namespace StringUtils
--- src/string_utils.cpp
+++ src/string_utils.cpp
@@ -1,63 +1,9 @@
 // strings.cpp
 #include "../include/string_utils.hpp"
-#include "../include/date_utils.hpp"
-#include "../include/file_utils.hpp"
-#include "../include/paths.hpp"
 #include <algorithm>
-#include <iostream>
 #include <string>
 
 namespace StringUtils {
-
-ReplacePairs GetReplacePairs(const std::string &project_name,
-                             const std::string &author_name) {
-  ReplacePairs pairs;
-
-  pairs[Placeholder::ProjectName] = project_name;
-  pairs[Placeholder::AuthorName] = author_name;
-  pairs[Placeholder::Date] = DateUtils::GetCurrentYearStr();
-
-  return pairs;
-}
-
-Contents GetContents(const LicenseType &license) {
-  Contents contents;
-
-  try {
-    contents[ContentType::CMake] = FileUtils::LoadFileToString(Paths::CMake);
-    contents[ContentType::Readme] = FileUtils::LoadFileToString(Paths::Readme);
-    contents[ContentType::Metadata] =
-        FileUtils::LoadFileToString(Paths::Metadata);
-    contents[ContentType::Main] = FileUtils::LoadFileToString(Paths::Main);
-
-    switch (license) {
-    case LicenseType::MIT: {
-      contents[ContentType::License] = FileUtils::LoadFileToString(Paths::MIT);
-      break;
-    }
-    case LicenseType::BSD: {
-      contents[ContentType::License] = FileUtils::LoadFileToString(Paths::BSD);
-      break;
-    }
-    case LicenseType::GPL: {
-      contents[ContentType::License] = FileUtils::LoadFileToString(Paths::GPL);
-      break;
-    }
-    case LicenseType::Apache: {
-      contents[ContentType::License] =
-          FileUtils::LoadFileToString(Paths::Apache);
-      break;
-    }
-    case LicenseType::Custom: {
-      break; // TODO:
-    }
-    }
-  } catch (const std::exception &e) {
-    std::cerr << "Error loading templates: " << e.what() << std::endl;
-  }
-
-  return contents;
-}
 
 std::string toLower(std::string s) {
     std::transform(s.begin(), s.end(), s.begin(),
