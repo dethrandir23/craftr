8708ff3724de4bf430ce4f77d1038a8cc58deaee
Bekir Efe Öztürk <efeoztrk23@proton.me>
2025-09-19 23:42:22 +0300

    feat: Add command execution system

--- include/project_utils.hpp
+++ include/project_utils.hpp
@@ -5,7 +5,7 @@
 #include <string>
 
 namespace ProjectUtils {
-bool create_project(const Config& config);
+bool create_project(const Config &config);
 /*bool add_module(const std::string& name);*/
 
 } // namespace ProjectUtils
--- src/project_utils.cpp
+++ src/project_utils.cpp
@@ -5,15 +5,15 @@
 #include <iostream>
 #include <string>
 
+#include "../include/config.hpp"
 #include "../include/file_utils.hpp"
 #include "../include/project.hpp"
 #include "../include/project_template.hpp"
 #include "../include/replacer_utils.hpp"
-#include "../include/config.hpp"
 
 namespace ProjectUtils {
 
-bool create_project(const Config& config) {
+bool create_project(const Config &config) {
   try {
     ProjectTemplate tmpl(config.templatePath);
     if (!tmpl.LoadTemplate()) {
@@ -31,11 +31,21 @@ bool create_project(const Config& config) {
       return false;
     }
 
+    project.createCommands();
+    if (!CommandUtils::run_commands(project.getCommands(),
+                                    project.getCommandMode())) {
+      std::cerr << "Failed to execute commands of template.\n";
+      return false;
+    } else {
+      std::cout << "Executed all commands with success." << std::endl;
+    }
+
   } catch (const std::exception &e) {
     std::cerr << "Error creating project: " << e.what() << std::endl;
     return false;
   }
-  std::cout << "Project '" << config.name << "' created successfully!" << std::endl;
+  std::cout << "Project '" << config.name << "' created successfully!"
+            << std::endl;
   return true;
 }
 } // namespace ProjectUtils
