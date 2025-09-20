c12ddbc13a9d357f3f940a5eb311becd48001435
Bekir Efe Öztürk <efeoztrk23@proton.me>
2025-09-20 18:35:47 +0300

    feat: Add colors.hpp for color codes

---
+++ include/colors.hpp
@@ -0,0 +1,7 @@
+// colors.hpp
+
+namespace Colors {
+inline constexpr const char *GREEN = "\033[32m";
+inline constexpr const char *RED = "\033[31m";
+inline constexpr const char *RESET = "\033[0m";
+} // namespace Colors
